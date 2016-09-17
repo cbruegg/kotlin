95c2f25213e4f3b16893d3397cc83a1862267b08
Christian <c.brueggemann94@gmail.com>
2016-09-17 13:00:37 +0200

    Add onEach template

--- libraries/tools/kotlin-stdlib-gen/src/templates/Aggregates.kt
+++ libraries/tools/kotlin-stdlib-gen/src/templates/Aggregates.kt
@@ -719,6 +719,34 @@ fun aggregates(): List<GenericFunction> {
         }
     }
 
+    templates add f("onEach(action: (T) -> Unit)") {
+        inline(true)
+
+        doc { f -> "Performs the given [action] on each ${f.element} and returns the receiver afterwards." }
+        returns("SELF")
+        body {
+            """
+            return apply { for (element in this) action(element) }
+            """
+        }
+        include(Maps, CharSequences)
+
+        typeParam(Generics) { "I: Iterable<T>" }
+        returns(Generics) { "I" }
+
+        doc(Sequences) { f -> "Applies the supplied [action] on each ${f.element} of the Sequence as they pass through it." }
+        inline(false, Sequences)
+        returns(Sequences) { "Sequence<T>" }
+        body(Sequences) {
+            """
+            return map {
+                f(it)
+                it
+            }
+            """
+        }
+    }
+
     templates add f("forEach(action: (T) -> Unit)") {
         inline(true)
 
